9aba0799ee669b0c3d53a9538b27662f7316195d
Fernando Pinto Lino <30969689+FernandoPintoL@users.noreply.github.com>
2026-07-22 00:19:37 -0400

    Remove frontend from tracking - separate repo

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AudioUploader from '@/components/AudioUploader'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen py-8">
-      <AudioUploader />
-    </main>
-  )
-}
